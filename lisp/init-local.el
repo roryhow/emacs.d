f431a9dafa492158e707e8ba5da8627686eb71dd
Rory How <rhow93@gmail.com>
2018-07-26 14:48:16 +0300

    turn off auto quoting in web-mode

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -7,21 +7,6 @@
 
 (package-initialize)
 
-;;(custom-set-variables
-;; custom-set-variables was added by Custom.
-;; If you edit it by hand, you could mess it up, so be careful.
-;; Your init file should contain only one such instance.
-;; If there is more than one, they won't work right.
-;; '(package-selected-packages
-;;  (quote
-;;  (yaml-mode web-mode jsx-mode exec-path-from-shell flycheck js2-mode))))
-
-;; (custom-set-faces
-;; custom-set-faces was added by Custom.
-;; If you edit it by hand, you could mess it up, so be careful.
-;; Your init file should contain only one such instance.
-;; If there is more than one, they won't work right.
-;; )
 
 ;; Set custom tab width
 (setq tab-width 2)
@@ -87,8 +72,9 @@
   ;;; http://web-mode.org/
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
   (setq web-mode-enable-auto-indentation nil)
+  (setq web-mode-enable-auto-quoting nil)
+  (setq web-mode-code-indent-offset 2))
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
 ;; for better jsx syntax-highlighting in web-mode
