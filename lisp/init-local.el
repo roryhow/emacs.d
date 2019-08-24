e9a3bc7d4e83d6bfddd933732b986455ac7a1c58
Rory How <me@rory.how>
2019-08-24 10:14:05 +0300

    shutting up the linter

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,3 +1,5 @@
+;;; init-local.el --- Local tweaks to config -*- lexical-binding: t -*-
+;;; Commentary:
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
