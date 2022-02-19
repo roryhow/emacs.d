860e1929063d04552ca8096028f77e87772bc2d3
Rory How <rory@mercari.com>
2022-02-19 13:19:06 +0200

    Add more custom config things

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -3,13 +3,26 @@
 ;;; Code:
 
 (maybe-require-package 'go-mode)
+(maybe-require-package 'go-projectile)
+(maybe-require-package 'gotest)
+
 (add-hook 'go-mode-hook 'lsp)
 (add-hook 'go-mode-hook #'lsp-deferred)
 
-(defun lsp-go-install-save-hooks ()
-  (add-hook 'before-save-hook 'gofmt-before-save)
-  (add-hook 'go-mode-hook #'lsp-go-install-save-hooks))
+(defun go-setup-hook ()
+  "Personal go-mode setup tasks."
 
+  ;; Code layout.
+  (setq tab-width 2 indent-tabs-mode 1) ; std go whitespace configuration
+  (add-hook 'before-save-hook 'gofmt-before-save) ; run gofmt on each save
+
+  ;; Shortcuts for common go-test invocations.
+  (let ((map go-mode-map))
+    (define-key map (kbd "C-c a") 'go-test-current-project)
+    (define-key map (kbd "C-c m") 'go-test-current-file)
+    (define-key map (kbd "C-c .") 'go-test-current-test)))
+
+(add-hook 'go-mode-hook 'go-setup-hook)
 (add-hook 'go-mode-hook #'yas-minor-mode)
 
 (provide 'init-go)
