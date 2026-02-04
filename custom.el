c4a0e45f8b8c896e58b09f6fdfd5660d900b31f7
rory how <rory.how@wolt.com>
2026-02-04 11:37:19 +0900

    package updates

--- custom.el
+++ custom.el
@@ -5,7 +5,32 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(monokai))
  '(package-selected-packages
-   '(cue-mode vterm protobuf-mode direnv monokai-theme envrc uptimes shfmt dotenv-mode osx-location sudo-edit puni paredit plantuml-mode terraform-mode docker-compose-mode dockerfile-mode docker yaml-mode sbt-mode scala-mode kotlin-mode reformatter toml-mode ruff-format flymake-ruff pip-requirements pet tide web-mode js-comint xref-js2 prettier-js typescript-mode js2-mode json-mode markdown-mode ibuffer-projectile projectile-ripgrep projectile flymake-actionlint github-review github-clone bug-reference-github yagist magit-todos magit git-link git-timemachine git-modes which-key highlight-escape-sequences whole-line-or-region move-dup page-break-lines multiple-cursors avy browse-kill-ring symbol-overlay rainbow-delimiters mode-line-bell vlf list-unicode-display unfill windswap switch-window lsp-ui lsp-mode marginalia embark-consult consult embark orderless vertico flycheck-color-mode-line flycheck ibuffer-vc rg wgrep diff-hl diredfl disable-mouse default-text-scale ns-auto-titlebar dimmer color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized command-log-mode scratch diminish gcmh exec-path-from-shell gnu-elpa-keyring-update seq company forge)))
+   '(0blayout avy browse-kill-ring bug-reference-github
+              color-theme-sanityinc-solarized
+              color-theme-sanityinc-tomorrow command-log-mode company
+              consult copilot copilot-chat cue-mode default-text-scale
+              diff-hl diminish dimmer diredfl direnv disable-mouse
+              docker docker-compose-mode dockerfile-mode dotenv-mode
+              embark embark-consult envrc exec-path-from-shell
+              flycheck flycheck-color-mode-line flycheck-golangci-lint
+              flymake-actionlint flymake-ruff gcmh git-link git-modes
+              git-timemachine github-clone github-review
+              gnu-elpa-keyring-update go-mode go-playground
+              go-projectile gotest highlight-escape-sequences
+              ibuffer-projectile ibuffer-vc js-comint js2-mode
+              json-mode kotlin-mode list-unicode-display lsp-metals
+              lsp-mode lsp-ui magit magit-todos marginalia
+              markdown-mode mode-line-bell monokai-theme move-dup
+              multiple-cursors ns-auto-titlebar orderless osx-location
+              page-break-lines paredit pet pip-requirements
+              plantuml-mode prettier-js projectile projectile-ripgrep
+              protobuf-mode puni rainbow-delimiters reformatter
+              restclient rg ruff-format sbt-mode scala-mode scratch
+              seq shfmt sudo-edit switch-window symbol-overlay
+              terraform-mode tide toml-mode typescript-mode unfill
+              uptimes vertico vlf vterm web-mode wgrep which-key
+              whole-line-or-region windswap xref-js2 yagist yaml-mode
+              yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
